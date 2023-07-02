d0e9ea7f69f90a0f76388f5f28a80d7ffc0cfdcd
Varun <varunkrishnankv2002@gmail.com>
2023-07-02 21:44:16 +0530

    Keeper Updated

--- src/app.jsx
+++ src/app.jsx
@@ -4,13 +4,16 @@ import Footer from "./components/footer";
 import Note from "./components/note";
 import notes from "./notes";
 
+const currentTime=new Date().getHours();
+
 function app() {
   return (
     <div>
       <Header />
       {notes.map((data) => {
-        <Note key={data.key} Title={data.title} Desc={data.content} />;
+        return <Note key={data.key} Sr={data.key} Title={data.title} Desc={data.content} />;
       })}
+      {currentTime>12 && <h1>Why the Fuck You Working</h1>}
       <Footer />
     </div>
   );
